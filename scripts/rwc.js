c13b4c0ac0fe37909a951bf7f72b79476445d39d
OnisBrown <Onis.Brown@hotmail.co.uk>
2019-10-25 14:47:41 +0100

    debug attempt

--- scripts/rwc.js
+++ scripts/rwc.js
@@ -989,7 +989,7 @@ function rwcActionGazeAtPosition(x, y, z, secs){
     }
   });
   rwcPoseTopic.publish(poseStamped);
-  console.log("Gaze pose published to " + rwcPoseTopic);
+  console.log("Gaze pose published to " + JSON.stringify(rwcPoseTopic));
   console.log(poseStamped);
   return goal;
 }
